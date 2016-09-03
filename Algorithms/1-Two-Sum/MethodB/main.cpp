942f14933b4e5426808b206d06ca8cc961a0ffe9
jokhan <jokhanmei@gmail.com>
2016-09-03 16:31:09 +0800

    Add a method B using map

---
+++ Algorithms/1-Two-Sum/MethodB/main.cpp
@@ -0,0 +1,53 @@
+/*
+ * author: jokhan
+ * date: 2016年 09月 03日 星期六 15:48:13 CST
+ * function: main.cpp
+ */
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum( vector<int>& nums, int target ) {
+		vector<int> result(2);
+		map<int, int> mapNums;
+		//*****copy the data from a vector into a map****//
+		for( int i = 0; i < nums.size(); ++ i ) {
+			mapNums [ nums[i] ] = i;
+		}
+		for( int i = 0; i < nums.size(); ++ i ) {
+			int complement = target - nums[i];
+			map<int, int>::iterator iter;
+			iter = mapNums.find( complement );
+			if ( iter != mapNums.end() && mapNums[complement] != i) {
+				result[0] = i;
+				result[1] = mapNums[complement];
+				return result;
+			}
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	vector<int> nums;
+	Solution solution;
+	int target;
+
+	//******data for testing******//
+	nums.push_back( 3 );
+	nums.push_back( 2 );
+	nums.push_back( 4 );
+	target = 6;
+
+	vector<int> result;
+	result = solution.twoSum( nums, target );
+	cout << "[" << result[0] << "," << result[1] << "]" << endl;
+
+	return 0;
+}
