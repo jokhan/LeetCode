c00132f8dd35e794e71fe95436c439be44331d21
jokhan <jokhanmei@gmail.com>
2016-09-09 09:44:42 +0800

    update method B

--- Algorithms/5-Longest-Palindromic-Substring/MethodB/main.cpp
+++ Algorithms/5-Longest-Palindromic-Substring/MethodB/main.cpp
@@ -14,7 +14,8 @@ class Solution {
 		string longestPalindrome( string s ) {
 			//string result = "";
 			int maxLength = 0;
-			int left = 0, right = 0;
+			int left = 0;
+			//int right = 0;
 			int len = 0;
 			for( int i = 0; i < s.length(); ++ i ) {
 				int len1 = indicatorPldr( s, i, i );
@@ -23,7 +24,7 @@ class Solution {
 				if( len > maxLength ) {
 					maxLength = len;
 					left = i - ( len - 1 ) / 2;
-					right = i + len / 2;
+					//right = i + len / 2;
 				}
 			}
 			return s.substr( left, maxLength);
@@ -33,13 +34,9 @@ class Solution {
 		int indicatorPldr( string s, int left, int right ) {
 			//以回文串的中心位置向两边扩散
 			//找到最长回文串
-			while( left >= 0 && right < s.length() ) {
-				if( s.at( left ) != s.at( right ) ) {
-					return right - left - 1;
-				} else {
+			while( left >= 0 && right < s.length() && s.at(left) == s.at( right ) ) {
 					-- left;
 					++ right;
-				}
 			}
 			return right - left - 1;
 		}
